08711fcf089abfde4c6460726caec7dd5e226a3d
galvadion <urashimasan@gmail.com>
2016-12-20 10:06:28 -0300

    Correccion de mensaje de error
    Se corrigue el mensaje de error de las validaciones

--- src/main/webapp/static/controllers/payment/payment.controller.js
+++ src/main/webapp/static/controllers/payment/payment.controller.js
@@ -98,7 +98,7 @@
         		if(response.success){
 	        		vm.isPromotion=response.data.valid;	
 	        		if(!vm.isPromotion){
-	        			$rootScope.doFlashMessage(response.data.validationError, "", "error", 6000);
+	        			$rootScope.doFlashMessage(response.data.validationMessage, "", "error", 6000);
 	        		}else{
 	        			vm.percentage=response.data.percentage;
 	        			vm.promotionCode=response.data.promotionCode;
